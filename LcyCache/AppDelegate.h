8eff927d14ecd65d991504b3fec13f627f79e55e
lcyu <liaochenyu@wepiao.com>
2015-08-06 10:37:30 +0800

    Initial Commit

---
+++ LcyCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LcyCache
+//
+//  Created by lcyu on 15/8/6.
+//  Copyright (c) 2015年 Lcyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LcyCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LcyCache
+//
+//  Created by lcyu on 15/8/6.
+//  Copyright (c) 2015年 Lcyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
